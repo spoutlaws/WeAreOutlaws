e44f8f19bcb045a314ede5130bf0630f26f53799
spoutlaws <31293673+spoutlaws@users.noreply.github.com>
2017-08-29 17:41:08 -0400

    Update index.js

--- mix/js/index.js
+++ mix/js/index.js
@@ -103,6 +103,21 @@ jQuery(function ($) {
                     loadTrack(index);
                 }
             }),
+            btnFadeOut = $('#btnFadeOut').click(function () {
+				var button = $(this);
+				if (playing) {
+					audio.animate({volume: 0}, 1000, function () {
+						audio.pause();
+						button.text("Fade In");
+					});
+				}
+				else {
+					audio.animate({volume: 1}, 1000, function () {
+						audio.play();
+						button.text("Fade Out");
+					});
+				}
+            }),
             li = $('#plList li').click(function () {
                 var id = parseInt($(this).index());
                 if (id !== index) {
